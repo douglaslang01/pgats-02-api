eee2fa36d6f0717f1ae10fec1c0e4eb373ec812b
douglaslang01 <douglas.lang@outlook.com>
2025-09-29 17:07:39 -0300

    Adicionando a estrutura de um teste de performance usando K6 para a criação de transferências

---
+++ test/rest/performance/transfers/createTransfer.test.js
@@ -0,0 +1,34 @@
+// Bibliotecas
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+// Aplicação
+const app = require('../../../app');
+const { getToken } = require('../helpers/authentication')
+
+export const options = {
+    iterations: 1
+};
+
+export default function () {
+
+    const token = getToken();
+    const url = app + '/transfers';
+    const payload = JSON.stringify({
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    };
+
+    let res = http.post(url, payload, params);
+
+    check(res, {
+        'Validar que o Status é 201': (r) => r.status === 201,
+    });
+
+    sleep(1);
+} 
