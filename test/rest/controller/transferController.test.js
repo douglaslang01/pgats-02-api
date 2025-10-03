f0105cd9d6b0b9be2b58ca39f5907bb7b1b48acc
douglaslang01 <douglas.lang@outlook.com>
2025-10-03 16:31:26 -0300

    Criando testes de transferencias para não favorecidos

--- test/rest/controller/transferController.test.js
+++ test/rest/controller/transferController.test.js
@@ -6,9 +6,13 @@ const { expect } = require('chai');
 // Aplicação
 const app = require('../../../app');
 const auth = require('../helpers/authentication')
+const { createUser } = require('../helpers/user');
 
 // Mock
 const transferService = require('../../../service/transferService');
+
+//Fixtures
+const postTransfer = require('../fixture/requisicoes/transferencias/postTransfer.json');
 
 // Testes
 describe('Transfer Controller', () => {
@@ -17,6 +21,46 @@ describe('Transfer Controller', () => {
         before(async () => {
             token = await auth.getToken();
         });
+
+        describe.only('Transferencias para não favorecidos', () => {
+            let newUser;
+            let reqTransfer;
+            before(async () => {
+                newUser = await createUser('maria');
+                reqTransfer = { ...postTransfer };
+                reqTransfer.to = newUser.username;
+            })
+
+            it('CT05.1 - Transferência de R$ 5.000,00 para não favorecido deve retornar 201', async () => {
+
+                //Arrange
+                reqTransfer.value = 5000.00;
+
+                //Action
+                const res = await request(app)
+                    .post('/transfers')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(reqTransfer);
+
+                //Assert
+                expect(res.status).to.equal(201);
+                expect(res.body).to.have.not.property('error');
+            });
+
+            it('CT05.2 - Transferência de R$ 5.000,01 para não favorecido deve retornar 400', async () => {
+
+                //Arrange
+                reqTransfer.value = 5000.01;
+
+                const res = await request(app)
+                    .post('/transfers')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send(reqTransfer);
+
+                expect(res.status).to.equal(400);
+                expect(res.body).to.have.property('error', 'Transferência acima de R$ 5.000,00 só para favorecidos');
+            });
+        })
 
         it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
             const resposta = await request(app)
