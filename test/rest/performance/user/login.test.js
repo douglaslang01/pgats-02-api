a29aa6e20d2b5fd447228770869911f94f207346
Bruna Gomes <bruna.ckgomes@gmail.com>
2025-10-08 23:01:36 -0300

    Incluíndo ajustes solicitados.

--- test/rest/performance/user/login.test.js
+++ test/rest/performance/user/login.test.js
@@ -1,17 +1,23 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { getBaseUrl } from '../../../utils/performance.js';
 
 
 export const options = {
-    iterations: 1,
+    //  iterations: 30,
+     stages: [
+    { duration: '20s', target: 200 },
+    { duration: '35s', target: 200 },
+    { duration: '5s', target: 0 },
+  ],
     thresholds: {
-        http_req_duration: ['p(95)<22', 'max<25'], // 95% das requisições devem ser respondidas em menos de 22ms
+        http_req_duration: ['p(95)<4000', ' max<6000'], // 95% das requisições devem ser respondidas em menos de 4000ms
         http_req_failed: ['rate<0.01'] // taxa de erro deve ser menor que 1%
     }
 };
 
 export default function () {
-    const url = 'http://localhost:3000/users/login';
+    const url = getBaseUrl() + '/users/login';
 
     const payload = JSON.stringify({//com essa função o objeto js vai ser transformado em JSON
         username: 'julio',
@@ -26,12 +32,10 @@ export default function () {
 
     const resposta = http.post(url, payload, params);
 
-    console.log(resposta.body);
-
     check(resposta, {
         'Validar que o status é 200': (r) => r.status === 200,
-        'Validar que o token é string': (r) => typeof(r.json().token) === 'string',
-        
+        'Validar que o token é string': (r) => typeof (r.json().token) === 'string',
+
     });
 
 
